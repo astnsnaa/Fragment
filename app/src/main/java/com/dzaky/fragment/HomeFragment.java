4b1094061f1123dbf69262d086e8877149b5fbe0
Muhammad Dzaky <mdzaky07@gmail.com>
2022-10-03 17:35:51 +0700

    first commit

---
+++ app/src/main/java/com/dzaky/fragment/HomeFragment.java
@@ -0,0 +1,30 @@
+package com.dzaky.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnCategory = view.findViewById(R.id.btn_category);
+        btnCategory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/dzaky/fragment/MainActivity.java
@@ -0,0 +1,29 @@
+package com.dzaky.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        HomeFragment mHomeFragment = new HomeFragment();
+        Fragment fragment = mFragmentManager.findFragmentByTag(HomeFragment.class.getSimpleName());
+
+        if (!(fragment instanceof HomeFragment)) {
+            Log.d("MyFlexibleFragment","Fragment Name :" + HomeFragment.class.getSimpleName());
+            mFragmentManager
+                    .beginTransaction()
+                    .add(R.id.frame_container, mHomeFragment, HomeFragment.class.getSimpleName())
+                    .commit();
+        }
+    }
+}
